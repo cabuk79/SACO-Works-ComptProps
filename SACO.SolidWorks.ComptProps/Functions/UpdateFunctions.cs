fbaa7badb7fc20964c9ee7e71756ec6f93a66466
cabuk79 <cabuk79@googlemail.com>
2023-04-28 10:02:38 +0100

    Added new folder Functions and class UpdateFunctions and inside there added a SaveProps method to save all the props usiing reflection through the PropsModel

---
+++ SACO.SolidWorks.ComptProps/Functions/UpdateFunctions.cs
@@ -0,0 +1,40 @@
+﻿using SACO.SolidWorks.ComptProps.Models;
+using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACO.SolidWorks.ComptProps.Functions
+{
+    public class UpdateFunctions
+    {
+        private SldWorks swApp;
+        private ModelDoc2 swModel;
+        private ModelDocExtension modelExt;
+        private CustomPropertyManager customProps;
+
+        SacoComponentPropertiesModel propsModel;
+
+        public UpdateFunctions(SldWorks SwApp, SacoComponentPropertiesModel PropsModel)
+        {
+            swApp = SwApp;
+            swModel = swApp.ActiveDoc;
+            propsModel = PropsModel;
+        }
+
+        public void SaveProps()
+        {
+            modelExt = swModel.Extension;
+            customProps = modelExt.CustomPropertyManager[""];
+
+            //loop through each property in propsmodel and set to the required property by doing a
+            foreach(var propItem in propsModel.GetType().GetProperties())
+            {
+                customProps.Set2(propItem.Name, propItem.GetValue(propItem, null).ToString());
+            }
+
+        }
+    }
+}
